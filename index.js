5cd6ee1972e86c73f8aa8821185b7fd64308e453
Diksha Dhote <dikshadhote7042@gmail.com>
2022-01-25 10:21:44 +0530

    feat: toggle baseline snackbar onclick

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-
+// Badge
 let hideButton=document.querySelector('.hide-btn');
 let hideBadge=document.getElementById('hide-badge');
-console.log(hideButton)
-console.log(hideBadge)
 
-const onHideClickHandler =() =>{
-console.log(hideBadge.style.display);
+// Snackabar
+let baselineButton=document.getElementById('baseline-btn');
+let baselineSnackbar=document.querySelector('#snackbar-baseline');
+
+console.log(baselineButton)
+console.log(baselineSnackbar)
+
+
+    
+const onHideClickHandler = () =>{
     if(hideBadge.style.display==="block")
     {
         console.log(hideBadge.style.display);
-        hideBadge.style.display="none";
+        hideBadge.style.display="none";   
     }
      else{
-        hideBadge.style.display="block";
+        hideBadge.style.display="block";  
     }
-    
-    }
-    
-hideButton.addEventListener("click",onHideClickHandler);
 
+}
+
+const onBaselineClickHandler =() =>{
+        if(baselineSnackbar.style.display==="flex")
+          {
+            console.log(hideBadge.style.display);
+             baselineSnackbar.style.display="none";   
+            }
+         else{
+          baselineSnackbar.style.display="flex";  
+             }
+       
+        }
+
+hideButton.addEventListener("click",onHideClickHandler);
+baselineButton.addEventListener("click",onBaselineClickHandler);
